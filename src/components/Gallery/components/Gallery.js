749a20d3349e1f3bdabd742141294fb96cdc4e52
tyautyau56 <i19yanagi@gmail.com>
2020-09-26 19:51:58 +0900

    add Gallery.js options

--- src/components/Gallery/components/Gallery.js
+++ src/components/Gallery/components/Gallery.js
@@ -21,6 +21,8 @@ const Gallery = ({ images = DEFAULT_IMAGES }) => {
           id={obj.id}
           source={obj.source}
           thumbnail={obj.thumbnail}
+          caption={obj.caption}
+          description={obj.description}
           position={obj.position}
           toggleLightbox={obj.toggleLightbox}
           position={i}
